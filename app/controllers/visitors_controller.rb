ac859f29039a6fed64085d027422d58f7cd2994b
Steven Inouye <steven.inouye91@gmail.com>
2014-09-16 20:45:24 -0700

    removed winnier

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -2,7 +2,6 @@ class VisitorsController < ApplicationController
 
 def create
   Visitor.create(email: params[:email], position: params[:position], company: params[:company])
-  return "winner"
 end
 
 end
